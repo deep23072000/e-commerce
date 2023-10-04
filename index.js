151fd938a93c5bb5b0e748830e5e13c0b2ccec5f
deep23072000 <114299553+deep23072000@users.noreply.github.com>
2023-10-04 19:45:35 +0530

    navbar upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function run(){
+    setTimeout(play,3000)
+
+    function play(){
+        document.getElementById('logo').style.display='block';
+        document.getElementById('firstlogo').style.display='none';
+
+    console.log("ghddgsh")
+    }
+}
